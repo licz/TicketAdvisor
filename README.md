62d4d4cf08e9e7f8e50868a4f4f0db270abf824f
licz <leszek.mayako@gmail.com>
2016-10-09 20:53:02 +0100

    another one

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TicketAdvisor
 
 App created to scan tickets on www.residentadvisor.com to find out if they are available (there is a resale system on RA).
+
 Use advisor.properties file to set up your email details.
--- src/main/java/com/lich/ticketAdvisor/utils/notifier/EmailNotifier.java
+++ src/main/java/com/lich/ticketAdvisor/utils/notifier/EmailNotifier.java
@@ -1,7 +1,6 @@
 package com.lich.ticketAdvisor.utils.notifier;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
